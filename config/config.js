f8a1df2dc109fd26248dcdcb3ecbe2822af807f8
Stefan Mueller <stmu@gmx.net>
2014-12-10 20:58:11 +0100

    refactored api

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    leds
+    {
+        colorIndex: {
+            red: 2,
+            green: 1,
+            blue: 0,
+        },
+        numLeds: 32,
+        spiDevice: "/dev/spidev0.0"
+    }
+};
